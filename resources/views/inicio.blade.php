f68160015da25df0437b99bc177161338f0ac80c
“07asier” <asier2223@gmail.com>
2017-11-19 15:53:32 +0100

    Initial commit

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>OBD Diagnostico</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link href="css/estiloshome.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Estilos del footer -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+
+</head>
+
+<body >
+
+<!-- Navigation -->
+<nav class="navbar-inverse" id="navbar">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#" id="titulo">Racegnostic</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="" id="menus">Inicio</a></li>
+                <li><a href="" id="menus">Quienes somos</a></li>
+                <li><a href="contact" id="menus">Contacto</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="register" id="menus"><span class="glyphicon glyphicon-user"></span> Registrarse</a></li>
+                <li><a href="login" id="menus"><span class="glyphicon glyphicon-log-in"></span> Iniciar sesión</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<header class="masthead text-center text-white d-flex">
+    <div class="container my-auto">
+        <div class="row">
+            <div >
+                <h1 class="text-uppercase">
+                    <p>OBTEN LA INFO DE TU COCHE.</p>
+                    <p>EN UN ABRIR Y CERRAR DE OJOS.</p>
+                </h1>
+
+            </div>
+            <div>
+
+                <p class="blanco">¿Quieres conocernos?</p>
+                <a class="btn btn-primary btn-lg active"  aria-pressed="true" href="#services">Descubrenos</a>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<section id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <hr class="my-4">
+                <h2 class="section-heading">¿Por qué nosotros?</h2>
+
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 text-center">
+                <div class="service-box mt-5 mx-auto">
+                    <img src="images/icons/fast.png"> </img>
+                    <h3>Rápido</h3>
+                    <p class="text-muted mb-0">Diagnóstico a tiempo real</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+                <div class="service-box mt-5 mx-auto">
+                    <img src="images/icons/security.png"> </img>
+                    <h3 >Fiable</h3>
+                    <p class="text-muted mb-0">Servicio 100% fiable</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+                <div class="service-box mt-5 mx-auto">
+                    <img src="images/icons/coche.png"> </img>
+                    <h3>Multifuncional</h3>
+                    <p class="text-muted mb-0">Ofrecemos la posibilidad de añadir varios coches</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+                <div class="service-box mt-5 mx-auto">
+                    <img src="images/icons/easy.png"> </img>
+                    <h3>Accesibilidad</h3>
+                    <p class="text-muted mb-0">Fácil acceso a los datos</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section>
+    <div class="container my-auto">
+        <div class="row">
+            <div>
+                <hr class="my-4">
+                <h3 class="text-center">Quiénes somos</h3>
+                <p class="text-center">Somos una pequeña empresa del ámbito automovilistico, que analizamos a tiempo real(a través de 4G) los datos de un coche.Mediante un <a href="https://es.wikipedia.org/wiki/OBD">OBD-II </a> almacenamos los datos en la <a href="https://es.wikipedia.org/wiki/Computaci%C3%B3n_en_la_nube" >nube</a>, y luego los plasmamos en ésta aplicación web. Es un servicio rápido, fiable y sobre todo sencillo de usar. Ofrecemos la opción de añadir más de un vehiculo a la aplicación. </p>
+                <hr class="my-4">
+                <h3 class="text-center">Cómo funciona</h3>
+                <p class="text-center"><li><strong>Paso 1:</strong></li> </br> Enchufar el OBD-II en la ranura del coche </p>
+                <p class="text-center"><li><strong>Paso 2:</strong></li> </br> Introducir el número de serie del OBD-II en el coche, y configurar los datos del coche. </p>
+                <p class="text-center"><li><strong>Paso 3:</strong></li> </br> Listo! </p>
+            </div>
+        </div>
+    </div>
+</section>
+<div id="footer1">
+</div>
+
+<footer class="footer p-t-1">
+    <div class="container">
+        <div class="pull-right">
+            <nav class="navbar" style="background:transparent; color: black;">
+                <nav class="nav navbar-nav izquierda">
+                    <a class="nav-item nav-link" href="#">Home</a>
+                    <a class="nav-item nav-link" href="#">Contacto</a>
+                    <p id="parrafo">Diagnostico Coches Team©</p>
+
+                </nav>
+            </nav>
+        </div>
+
+        <a href="http:\\facebook.es"><i class="fa fa-facebook-official fa-2x"></i></a>
+        <a href="http:\\pinterest.com"><i class="fa fa-pinterest-p fa-2x"></i></a>
+        <a href="http:\\twitter.com"><i class="fa fa-twitter fa-2x"></i></a>
+        <a href="http:\\flickr.com"><i class="fa fa-flickr fa-2x"></i></a>
+        <a href="http:\\linkedin.com"><i class="fa fa-linkedin fa-2x"></i></a>
+
+        <p class="h6">
+            Hecho por: Julen,Sergio y Asier-2017
+            <a href="http://www.paradigmOz.com" target="_blank"></a>
+        </p>
+    </div>
+</footer>
+
+
+</body>
+
+
+</html>
